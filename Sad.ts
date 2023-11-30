ea3e90e97972ba7390c790d8b15fcf54808c5de6
Seo Dong min <gundam3331@knou.ac.kr>
2023-12-01 03:50:34 +0900

    ts

---
+++ Sad.ts
@@ -0,0 +1,25 @@
+class Emotion {
+  #imSad: boolean;
+  constructor() {
+    this.#imSad = true;
+  }
+  sad(): boolean {
+    process.stdout.write("When I get sad, ");
+    return this.#imSad;
+  }
+  sadStop(): void {
+    this.#imSad = false;
+    process.stdout.write("I stop being sad ");
+  }
+  beAwesome(): void {
+    process.stdout.write("and be awesome instead");
+  }
+}
+
+const myEmotion: Emotion = new Emotion();
+
+// life motto
+if (myEmotion.sad() === true) {
+  myEmotion.sadStop();
+  myEmotion.beAwesome();
+}
